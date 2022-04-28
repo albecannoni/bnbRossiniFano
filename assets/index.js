4353e6b0f841f1b2a0d5a24a44ae71b737607ea3
albecannoni <alberto.cannoni@outlook.it>
2022-04-29 00:07:43 +0200

    creazione pagina contatti

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,4 @@
+//variabili globali
 let nome = document.querySelector('#fname');
 let cognome = document.querySelector('#lname');
 let posta = document.querySelector('#posta');
@@ -5,12 +6,15 @@ let telefono = document.querySelector('#telephone');
 let testo = document.querySelector('#info')
 let btn = document.querySelector('#infoBtn');
 let errore = document.querySelector('#alert');
-let totInfo = []
-
+let pool = document.querySelector('#pool');
+let contactPage = document.querySelector('#contactPage');
+let roomPage = document.querySelector('#roomPage');
+let servicesPage = document.querySelector('#servicesPage');
+let contatti = document.querySelector('#listaContatti');
 
 window.addEventListener('load', init);
 
-/* pulizia campi e listener bottone form*/
+// pulizia campi e listener bottone form
 function init() {
     nome.value = '';
     cognome.value = '';
@@ -19,6 +23,9 @@ function init() {
     testo.value = '';
     errore.innerHTML = '';
     btn.addEventListener('click', check);
+    contactPage.addEventListener('click', contactPageGen);
+    roomPage.addEventListener('click', roomPageGen)
+    servicesPage.addEventListener('click', servicesPageGen)
 }
 
 /*controllo form*/
@@ -50,6 +57,27 @@ function check() {
 }
 
 /*============================================= */
+function svuotaPool() {
+    pool.innerHTML = ``;
 
+}
+function contactPageGen() {
+    svuotaPool();
+    newDiv = document.createElement("div");
+    newDiv.setAttribute('id', 'listaContatti');
+    pool.innerHTML = `<div id="elencocontatti" class="pt-5 d-flex justify-content-center align-items-center"></div>`;
+    boxContatti = document.querySelector('#elencocontatti');
+    boxContatti.classList.add("bg-danger", "corsivo");
+    boxContatti.innerHTML = `<h3>testo di prova</h3>`;
+    console.log('pagina contatti generata');
+}
+
+function roomPageGen() {
+    console.log('pagina stanze generata');
+}
+
+function servicesPageGen() {
+    console.log('pagina servizi generata');
+}
 
 
