27bbc790d6ba594abbe513cbeea43c330ba5ab79
albecannoni <alberto.cannoni@outlook.it>
2022-05-23 16:29:32 +0200

    modifica button form

--- assets/index.js
+++ assets/index.js
@@ -363,7 +363,7 @@ function formGen() {
                                 </fieldset>
 
                                 <div class="d-flex justify-content-center align-items-center py-2">
-                                    <button type="button" value="send" class="bg-viola bRadius text-white " id="infoBtn">
+                                    <button type="button" class="bg-viola bRadius text-white " id="infoBtn">
                                         <div class="svg-wrapper-1">
                                             <div class="svg-wrapper">
                                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
